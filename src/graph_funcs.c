RaphaelNeubert/pl0-compiler

commit f542813099fb8526ded474ffb4bb3decd461fc46

extended if statements with else branch

src/graph_funcs.c
@@ -19,9 +19,6 @@ static struct stack_head *jump_pos_stack; //used for jpos and proc calls
 
 static int condition_operator;
 
-// required because call needs to be generated after parameter
-static int call_proc_num;
-
 
 void init_namelist()
 {
@@ -548,7 +545,22 @@ int stmnt_if_jnot()
 int stmnt_if_jaddr()
 {
     char *pjnot=stack_pop(jump_pos_stack);
-    //jump starts after relative addr parameter thats why -2
+    //jump starts after relative addr parameter 
+    //also note that we need to jump behind the jmp of the else
+    short rel_addr=cbuf_curr-pjnot; 
+    write_code_at(rel_addr, pjnot+1);
+    return 1;
+}
+int stmnt_else_jmp()
+{
+    stack_push(jump_pos_stack, cbuf_curr);
+    generate_code(jmp, 0);
+    return 1;
+}
+int stmnt_else_jaddr()
+{
+    char *pjnot=stack_pop(jump_pos_stack);
+    //jump starts after relative addr parameter 
     short rel_addr=cbuf_curr-pjnot-3; 
     write_code_at(rel_addr, pjnot+1);
     return 1;

src/graph.c
@@ -50,37 +50,39 @@ static struct Edge g_statement[] = {
 // a := b+c
 /*0*/ {EtMo, {(ul)mcIdent},    stmnt_assign, 1,  3},
 /*1*/ {EtSy, {(ul)tErg},       NULL, 2,  0},
-/*2*/ {EtGr, {(ul)g_expr},     stmnt_store, 25,  0},
+/*2*/ {EtGr, {(ul)g_expr},     stmnt_store, 27,  0},
 // if
-/*3*/ {EtSy, {(ul)tIF},        NULL, 4,  7},
+/*3*/ {EtSy, {(ul)tIF},        NULL, 4,  9},
 /*4*/ {EtGr, {(ul)g_cond},     stmnt_if_jnot, 5,  0},
 /*5*/ {EtSy, {(ul)tTHN},       NULL, 6,  0},
-/*6*/ {EtGr, {(ul)g_statement},stmnt_if_jaddr, 25,  0},
+/*6*/ {EtGr, {(ul)g_statement},stmnt_if_jaddr, 7,  0},
+/*7*/ {EtSy, {(ul)tELS},       stmnt_else_jmp, 8,  27},
+/*8*/ {EtGr, {(ul)g_statement},stmnt_else_jaddr, 27,  0},
 // while
-/*7*/ {EtSy, {(ul)tWHL},       stmnt_while_jback, 8,  11},
-/*8*/ {EtGr, {(ul)g_cond},     stmnt_while_jnot, 9,  0},
-/*9*/ {EtSy, {(ul)tDO},        NULL, 10, 0},
-/*10*/ {EtGr, {(ul)g_statement},stmnt_while_jaddr, 25,  0},
+/*9*/  {EtSy, {(ul)tWHL},       stmnt_while_jback, 10,  13},
+/*10*/ {EtGr, {(ul)g_cond},     stmnt_while_jnot, 11,  0},
+/*11*/ {EtSy, {(ul)tDO},        NULL, 12, 0},
+/*12*/ {EtGr, {(ul)g_statement},stmnt_while_jaddr, 27,  0},
 // begin
-/*11*/{EtSy, {(ul)tBGN},       NULL, 12, 15},
-/*12*/{EtGr, {(ul)g_statement},NULL, 13, 0},
-/*13*/{EtSy, {(ul)';'},        NULL, 12,  14},
-/*14*/{EtSy, {(ul)tEND},       NULL, 25, 0},
+/*13*/{EtSy, {(ul)tBGN},       NULL, 14, 17},
+/*14*/{EtGr, {(ul)g_statement},NULL, 15, 0},
+/*15*/{EtSy, {(ul)';'},        NULL, 14,  16},
+/*16*/{EtSy, {(ul)tEND},       NULL, 27, 0},
 // call
-/*15*/{EtSy, {(ul)tCLL},       NULL, 16, 21},
-/*16*/{EtMo, {(ul)mcIdent},    stmnt_call, 17,  0},
-/*17*/{EtSy, {(ul)'('},        NULL, 18, 0},
-/*18*/{EtGr, {(ul)g_expr},     stmnt_call_param, 19, 20},
-/*19*/{EtSy, {(ul)','},        NULL, 18, 20},
-/*20*/{EtSy, {(ul)')'},        stmnt_call_end, 25, 0},
+/*17*/{EtSy, {(ul)tCLL},       NULL, 18, 23},
+/*18*/{EtMo, {(ul)mcIdent},    stmnt_call, 19,  0},
+/*19*/{EtSy, {(ul)'('},        NULL, 20, 0},
+/*20*/{EtGr, {(ul)g_expr},     stmnt_call_param, 21, 22},
+/*21*/{EtSy, {(ul)','},        NULL, 20, 22},
+/*22*/{EtSy, {(ul)')'},        stmnt_call_end, 27, 0},
 // ?
-/*21*/{EtSy, {(ul)'?'},        NULL, 22, 23},
-/*22*/{EtMo, {(ul)mcIdent},    stmnt_get, 25,  0},
+/*23*/{EtSy, {(ul)'?'},        NULL, 24, 25},
+/*24*/{EtMo, {(ul)mcIdent},    stmnt_get, 27,  0},
 // !
-/*23*/{EtSy, {(ul)'!'},        NULL, 24, 25},
-/*24*/{EtGr, {(ul)g_expr},     st_putval, 25,  0},
+/*25*/{EtSy, {(ul)'!'},        NULL, 26, 27},
+/*26*/{EtGr, {(ul)g_expr},     st_putval, 27,  0},
 
-/*25*/{EtEn, {(ul)0},          NULL, 0,  0} 
+/*27*/{EtEn, {(ul)0},          NULL, 0,  0} 
 };
 
 struct Edge g_prog[] = {

src/graph_funcs.h
@@ -54,6 +54,8 @@ int stmnt_store();
 int stmnt_get();
 int stmnt_if_jaddr();
 int stmnt_if_jnot();
+int stmnt_else_jaddr();
+int stmnt_else_jmp();
 int stmnt_while_jaddr();
 int stmnt_while_jnot();
 int stmnt_while_jback();

commit 247589b28fc1f52e07739029d3d1495722554f14

fixed bug introduced by f542813099fb8526ded474ffb4bb3decd461fc46 that let if's without elses cause a segfault in the vm

src/graph_funcs.c
@@ -549,20 +549,20 @@ int stmnt_if_jaddr()
     //also note that we need to jump behind the jmp of the else
     short rel_addr=cbuf_curr-pjnot; 
     write_code_at(rel_addr, pjnot+1);
+    generate_code(jmp, 0); // jump for else
     return 1;
 }
 int stmnt_else_jmp()
 {
     stack_push(jump_pos_stack, cbuf_curr);
-    generate_code(jmp, 0);
     return 1;
 }
 int stmnt_else_jaddr()
 {
     char *pjnot=stack_pop(jump_pos_stack);
     //jump starts after relative addr parameter 
-    short rel_addr=cbuf_curr-pjnot-3; 
-    write_code_at(rel_addr, pjnot+1);
+    short rel_addr=cbuf_curr-pjnot; 
+    write_code_at(rel_addr, pjnot-2);
     return 1;
 }
 //st5